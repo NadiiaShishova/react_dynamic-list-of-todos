372a0ad7eebb429d560f3616979b20e209268a36
Nadiia Shishova <nadin71983@gmail.com>
2026-01-08 23:57:44 +0200

    add task solution

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 // eslint-disable-next-line import/extensions
 import { Todo } from '../types/Todo';
+import classNames from 'classnames';
 
 type Props = {
   todos: Todo[];
@@ -27,7 +28,9 @@ export const TodoList: React.FC<Props> = ({ todos, onSelect }) => (
         <tr
           key={todo.id}
           data-cy="todo"
-          className={todo.completed ? 'has-background-info-light' : ''}
+          className={classNames({
+            'has-background-info-light': todo.completed,
+          })}
         >
           <td className="is-vcentered">{todo.id}</td>
 
@@ -41,9 +44,10 @@ export const TodoList: React.FC<Props> = ({ todos, onSelect }) => (
 
           <td className="is-vcentered is-expanded">
             <p
-              className={
-                todo.completed ? 'has-text-success' : 'has-text-danger'
-              }
+              className={classNames({
+                'has-text-success': todo.completed,
+                'has-text-danger': !todo.completed,
+              })}
             >
               {todo.title}
             </p>
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import classNames from 'classnames';
 import { getUser } from '../../api';
 import { Todo } from '../../types/Todo.js';
 import { User } from '../../types/User.js';
@@ -22,7 +23,7 @@ export const TodoModal: React.FC<Props> = ({ todo, onClose }) => {
   }, [todo.userId]);
 
   return (
-    <div className="modal is-active" data-cy="modal">
+    <div className={classNames('modal', { 'is-active': true })} data-cy="modal">
       <div className="modal-background" />
 
       {loading || !user ? (
@@ -52,9 +53,10 @@ export const TodoModal: React.FC<Props> = ({ todo, onClose }) => {
 
             <p className="block" data-cy="modal-user">
               <strong
-                className={
-                  todo.completed ? 'has-text-success' : 'has-text-danger'
-                }
+                className={classNames({
+                  'has-text-success': todo.completed,
+                  'has-text-danger': !todo.completed,
+                })}
               >
                 {todo.completed ? 'Done' : 'Planned'}
               </strong>
